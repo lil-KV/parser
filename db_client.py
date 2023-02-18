0bdf7c4be82fc8695218ea2048bac9d27033a3e6
Liliya <lilya.kazak1@mail.ru>
2023-02-18 23:38:02 +0300

    added db (need to fix price)

--- db_client.py
+++ db_client.py
@@ -1,1 +1,33 @@
 # одинаковый для всех ссылок
+import psycopg2
+
+DBNAME = 'postgres'
+USER = 'postgres'
+PASSWORD = 'lk1118740'
+HOST = '127.0.0.1'
+
+FLATS_TABLE = 'flats'
+
+
+def create_flats_table():
+    with psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST) as conn:
+        with conn.cursor() as cur:
+            cur.execute('''
+            CREATE TABLE IF NOT EXISTS flats(
+                id serial,
+                link CHARACTER VARYING(300),
+                reference CHARACTER VARYING(30),
+                price INTEGER,
+                title CHARACTER VARYING(1000),
+                description CHARACTER VARYING(3000),
+                date TIMESTAMP WITH TIME ZONE)''')
+
+
+def insert_flat(flat):
+    with psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST) as conn:
+        with conn.cursor() as cur:
+            cur.execute('''INSERT INTO flats (link, reference, price, title, description, date) 
+            VALUES (%s, %s, %s, %s, %s, %s)''',
+                        (flat.link, flat.reference, flat.price, flat.title, flat.description, flat.date)
+                        )
+
--- gohome_parser.py
+++ gohome_parser.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from pars_model import Flat
 import re
 from datetime import datetime
+import db_client
 
 PARSER_NAME = 'gohome.by'  # reference
 
@@ -23,7 +24,12 @@ def turn_links_to_flats(links):
         resp = requests.get(link)
         html = BeautifulSoup(resp.content, 'html.parser')
         title = html.find('h1').text.strip()
-        price = re.sub('[^0-9]', '', html.find(class_='price big').text.strip()) # что не цифра, становится пробелом
+
+        raw_price = html.find('span', class_='price big')
+        if raw_price is not None:
+            price = re.sub('[^0-9]', '', raw_price.text.strip()) # что не цифра, становится пробелом
+        else:
+            price = 000
         description = html.find('p').text.strip()
         date = datetime.strptime(html.find_all(class_='description')[5].text.strip(), '%d.%m.%Y')
         number = re.sub('[^0-9]', '', html.find('a', class_='phone__link').text.strip())
@@ -41,8 +47,10 @@ def turn_links_to_flats(links):
     return flats
 
 
-def save_flats():
-    pass
+# функция для загрузки данных квартиры в БД
+def save_flats(flats):
+    for flat in flats:
+        db_client.insert_flat(flat)
 
 
 def get_last_flats(page_from=0, page_to=1):
@@ -53,7 +61,4 @@ def get_last_flats(page_from=0, page_to=1):
 
 
 
-
-
-get_all_flats_links()
 get_last_flats()
